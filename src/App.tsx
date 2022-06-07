66677c4de8933e5c62fcf9d09583fac8d1b88ef2
ismaelnt <ismaelnt97@gmail.com>
2022-06-07 11:46:33 -0300

    feat: add TodoForm

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+import common from './styles/common.module.scss';
+
 import { Header } from './components/Header';
+import { TodoForm } from './components/TodoForm';
 
 export function App() {
   return (
     <div>
       <Header />
+
+      <div className={common.container}>
+        <TodoForm />
+      </div>
     </div>
   );
 }
---
+++ src/components/TodoForm/index.tsx
@@ -0,0 +1,19 @@
+import { PlusCircle } from 'phosphor-react';
+
+import styles from './addTodo.module.scss';
+
+export function TodoForm() {
+  return (
+    <form className={styles.form}>
+      <input
+        type='text'
+        placeholder='Adicione uma nova tarefa'
+      />
+      <button
+        type='submit'
+      >
+        Criar <PlusCircle size={18} />
+      </button>
+    </form>
+  );
+}
